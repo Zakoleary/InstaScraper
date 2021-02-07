52943eed20c654ebad968dd5590f4a245226286c
Zakoleary <zakoleary@gmail.com>
2021-02-07 20:56:25 +0000

    Start of the scraper
    Selenium functioning as expected. 
    Removes the need to click the 'Accept' for cookie selection. 
    Username and password accepter via send_keys with .click().
    Auto Login is done. 
    Turn on notifications is also dismissed using XPATH plus the button element 'Turn On'.

---
+++ InstagramScraper.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 20:41:59 2021
+
+@author: zakol
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+import os
+import wget
+
+driver = webdriver.Chrome('C:/Users/zakol/Documents/ds_salary_proj/chromedriver.exe')
+driver.get("https://www.instagram.com/")
+
+Accept = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Accept')]"))).click()
+
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+username.clear()
+password.clear()
+username.send_keys("")
+password.send_keys("")
+
+log_in = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
+Accept = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Turn On')]"))).click()
